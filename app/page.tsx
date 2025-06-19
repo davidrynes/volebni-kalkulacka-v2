82b50bf7c3fc91b6a08bb82d1ae6fa2c5af16745
davidrynes <labs@davidrynes.com>
2025-06-19 10:53:06 +0200

    Úprava aplikace pro kompatibilitu s Preactem - vypnutí React Server Components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import dynamic from 'next/dynamic';
 
 // Dynamický import komponenty s vypnutím SSR
--- next.config.js
+++ next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, { dev, isServer }) => {
+    // Nahrazení Reactu za Preact
+    if (!isServer) {
+      Object.assign(config.resolve.alias, {
+        'react/jsx-runtime': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+    return config;
+  },
+  // Vypnutí React Server Components
+  experimental: {
+    serverActions: false
+  }
 };
 
 module.exports = nextConfig; 
