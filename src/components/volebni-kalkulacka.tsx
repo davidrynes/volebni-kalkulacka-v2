47ff59273a6277a77eace24589df37ce2cb2f3c7
davidrynes <labs@davidrynes.com>
2025-07-09 10:43:00 +0200

    Debug: Přidání debug informací pro otázky

--- src/components/volebni-kalkulacka.tsx
+++ src/components/volebni-kalkulacka.tsx
@@ -727,7 +727,11 @@ export function VolebniKalkulacka({ otazky, odpovedi = {}, stranyOdpovedi, bodov
       <div className="content-area" ref={contentAreaRef}>
         <div className="question-slide">
           <div className="question-text">
-            {currentQuestion?.text}
+            {currentQuestion?.text || "CHYBA: Otázka se nenačetla"}
+          </div>
+          {/* Debug info */}
+          <div style={{fontSize: '12px', color: 'red', padding: '5px'}}>
+            Debug: currentQuestionIndex={currentQuestionIndex}, totalQuestions={totalQuestions}, currentQuestion exists={!!currentQuestion}
           </div>
           
           {currentQuestion?.popis && (
